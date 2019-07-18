02e1c39391c3351851e2b63973385ba51d83bb70
Ilya Bychkov <18483i@gmail.com>
2019-07-18 12:18:45 +0100

    Automate the login to Google Drive

--- Drive_Send.py
+++ Drive_Send.py
@@ -2,13 +2,12 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 
-filepath = r""
-
+filename = r"C:\Users\il184\Desktop\CSVfile_2019-05-30.csv"
 g_login = GoogleAuth()
 g_login.LocalWebserverAuth()
 drive = GoogleDrive(g_login)
 
-with open(filepath, "r") as file:
+with open(filename, "r") as file:
     file_drive = drive.CreateFile({'title':os.path.basename(file.name) })  
     file_drive.SetContentString(file.read()) 
     file_drive.Upload()
