fa410114d6555dad8dc3934a1429496534dcdd94
Ilya Bychkov <18483i@gmail.com>
2019-07-19 13:44:16 +0100

    Add functionality for the upload function to save the credentials

--- Drive_Send.py
+++ Drive_Send.py
@@ -1,15 +1,32 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
+from datetime import datetime, timedelta
 
-filename = r"C:\Users\il184\Desktop\CSVfile_2019-05-30.csv"
-g_login = GoogleAuth()
-g_login.LocalWebserverAuth()
-drive = GoogleDrive(g_login)
+yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
 
-with open(filename, "r") as file:
-    file_drive = drive.CreateFile({'title':os.path.basename(file.name) })  
-    file_drive.SetContentString(file.read()) 
-    file_drive.Upload()
+filename = r"C:\Users\il184\Desktop\CSVfile_{0}.csv".format(yesterday)
 
-print("Files have been uploaded.")
+def upload_func(f_name):
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile("mycreds.txt")
+    if gauth.credentials is None:
+    # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+    # Refresh them if expired
+        gauth.Refresh()
+    else:
+    # Initialize the saved creds
+        gauth.Authorize()
+# Save the current credentials to a file
+    gauth.SaveCredentialsFile("mycreds.txt")
+    drive = GoogleDrive(gauth)
+    with open(f_name, "r") as file:
+        file_drive = drive.CreateFile({'title':os.path.basename(file.name) })  
+        file_drive.SetContentString(file.read()) 
+        file_drive.Upload()
+    with open("Log_file.txt", "a") as log_file:
+        log_file.write("{} has been uploaded.\n".format(f_name))
+
+upload_func(filename)
