5674d0fb92a40c63f82601ef6b9f6dbd4ed2465b
Ilya Bychkov <18483i@gmail.com>
2019-07-19 14:15:55 +0100

    Add "save to a specific folder" functionality

--- Drive_Send.py
+++ Drive_Send.py
@@ -6,27 +6,27 @@ from datetime import datetime, timedelta
 yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
 
 filename = r"C:\Users\il184\Desktop\CSVfile_{0}.csv".format(yesterday)
+folder_name = "csv_files"
 
-def upload_func(f_name):
+def upload_func(f_name, folder_name):
     gauth = GoogleAuth()
     gauth.LoadCredentialsFile("mycreds.txt")
     if gauth.credentials is None:
-    # Authenticate if they're not there
         gauth.LocalWebserverAuth()
     elif gauth.access_token_expired:
-    # Refresh them if expired
         gauth.Refresh()
     else:
-    # Initialize the saved creds
         gauth.Authorize()
-# Save the current credentials to a file
     gauth.SaveCredentialsFile("mycreds.txt")
     drive = GoogleDrive(gauth)
-    with open(f_name, "r") as file:
-        file_drive = drive.CreateFile({'title':os.path.basename(file.name) })  
-        file_drive.SetContentString(file.read()) 
-        file_drive.Upload()
+    folders = drive.ListFile({'q': "title='" + folder_name + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+    for folder in folders:
+        if folder['title'] == folder_name:
+            with open(f_name, "r") as file:
+                file_drive = drive.CreateFile({'title':os.path.basename(file.name), 'parents': [{'id': folder['id']} ] })
+                file_drive.SetContentString(file.read()) 
+                file_drive.Upload()
     with open("Log_file.txt", "a") as log_file:
         log_file.write("{} has been uploaded.\n".format(f_name))
 
-upload_func(filename)
+upload_func(filename, folder_name)
